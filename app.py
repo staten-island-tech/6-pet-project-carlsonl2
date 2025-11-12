3de50b1f50f6738e84c5b2713ea6625398781123
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-12 06:13:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+
+
 class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
         self.inventory = inventory
         
-            
-            
-    
+        
     def buy(self, item):
         self.inventory.append(item)
-        
+   
 
 Jordan = Hero("Jordan", 100, ["Wooden Sword"])
-alive = True
-while alive == True:
-    
+Shop = [{ "name" = "Iron Sword", "attack" = 35
+         
+         }]
+Jordan.buy({})
+
+
+        
+        
 
 
