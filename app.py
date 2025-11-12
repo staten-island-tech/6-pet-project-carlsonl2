2fa4c27021fa0c61981e898eb81074f740b76e34
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-12 05:55:16 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,17 @@ class Hero:
         self.name = name
         self.money = money
         self.inventory = inventory
-        work = input("Work Options : Coding")
-        if work == "Coding":
-            self.money += 15
+        
+            
+            
     
     def buy(self, item):
         self.inventory.append(item)
         
 
 Jordan = Hero("Jordan", 100, ["Wooden Sword"])
-
-class BankAccount:
-    def __init__(self, owner, balance):
-        self.owner = owner
-        self.__balance = balance  # double underscore means "private"
-       
+alive = True
+while alive == True:
     
-    def deposit(self, amount):
-        self.__balance += amount
 
-    def show_balance(self):
-        print(f"{self.owner} has ${self.__balance}")
 
