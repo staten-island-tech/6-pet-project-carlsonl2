ec68157a2b8b2c4fcbe0f5c22ae60c827c47c1eb
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-17 06:25:14 -0800

    Update pet.py

---
+++ pet.py
@@ -0,0 +1,19 @@
+class Pet:
+    def __init__(self, name, happiness):
+        self.name = name
+        self.happiness = happiness
+    def play(self, value):
+        self.happiness += value
+    def show(self):
+        print(f"{self.name}, {self.happiness}")
+
+Name = input("What would you like to name your pet?")
+Name = Pet(f"{Name}", 100)
+
+Living = True
+while Living == True:
+    Options = input("What would you like to do? 1.Play")
+    if Options.lower() == "play":
+        Name.play(10)
+        print(Name.show(Name))
+                                                                                                                                                       
