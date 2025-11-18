043d16b8fecc0b7c582a455db5a0da4d711e7b35
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-18 06:26:22 -0800

    Update password.py

--- password.py
+++ password.py
@@ -1,7 +1,18 @@
-def password(Email):
-    accepted = False
-    while accepted == False:
-        if "@" not in Email:
-            print("Email must contain an @ symbol, try again")
-password("carlsonl2")
+def password(Email, Password):
+    Characters = 0
+
+    if "@" not in Email:
+        print("Email must contain an @ symbol, try again")
+    elif "@" in Email:
+        print("Create a PassWord")
+        for i in Password:
+            Characters += 1
+        if Characters < 8:
+            print("Your password must be atleast 8 characters long")
+        if str.isdigit(Email):
+            
+
+            
+           
+password("carlson@", "P32ikeo")
 
