d65e6848f34e6fc0dfe9cc94f09c7d9e768cc4f6
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-19 06:21:47 -0800

    Update password.py

--- password.py
+++ password.py
@@ -1,18 +1,24 @@
-def password(Email, Password):
+def password(Email):
     Characters = 0
 
     if "@" not in Email:
-        print("Email must contain an @ symbol, try again")
-    elif "@" in Email:
-        print("Create a PassWord")
-        for i in Password:
-            Characters += 1
-        if Characters < 8:
-            print("Your password must be atleast 8 characters long")
-        if str.isdigit(Email):
+        return "Not Wanid email"
+    if Email != str:
+        return "Email must be a string"
+    '''for i in Password:
+        Characters += 1
+    if Characters < 8:
+        print("Your password must be atleast 8 characters long")
+    if not str(Password).isdigit:
+        print("Password must have 1 number")'''
+        
+
             
 
             
            
-password("carlson@", "P32ikeo")
+password("carlson")
+
+
+
 
