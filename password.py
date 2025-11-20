0cf09fc001ceee8ce78de7b94b53ce4e5ed2370d
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-20 06:16:00 -0800

    Update password.py

--- password.py
+++ password.py
@@ -1,19 +1,27 @@
-def password(Email):
-    '''Characters = 0'''
+def password(Email, Password):
+    
 
     if "@" not in Email:
         print("not a valid email")
     if not isinstance(Email, str):
         print("Email must be a string")
-    '''for i in Password:
+    if not isinstance(Password, str):
+         print("Password must be a string")
+    for i in Password:
+        Characters = 0
+        Special_Characters = ["!", "@"]
         Characters += 1
     if Characters < 8:
         print("Your password must be atleast 8 characters long")
-    if not str(Password).isdigit:
-        print("Password must have 1 number")'''
-        
+    if not any(Password.isdigit() for i in Password):
+        print("Password must have 1 number")
+    if not any(Password.isupper() for i in Password):
+        print("Password must have atlest 1 uppercase letter")
+    if not any(a in Password for a in Special_Characters):
+            print("Password must contain atlest 1 special character")
 
-         
+
+password("DFSDFSD@", ["askjshdjk"])         
 
             
            
