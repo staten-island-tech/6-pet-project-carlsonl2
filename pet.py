62eecae675b1132efff761293156515dab624094
carlsonl2 <carlsonl2@nycstudents.net>
2025-11-25 09:21:53 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,43 +1,34 @@
 class Pet:
-    def __init__(self, name, happiness, hunger):
+    def __init__(self, name, happiness, hunger, alive):
         self.name = name
         self.happiness = happiness
         self.hunger = hunger
+        if self.hunger < 25:
+            print("Your pet is hungry feed him soon!")
+        elif self.hunger > 150:
+             print("Your pet is very full, stop feeding him!")
+        
+            
     def play(self, value):
         self.happiness += value
         print(f"happiness has increased to {self.happiness}")
     def feed(self, value):
         self.hunger += value
+        print(f"Your pets hunger increased to {self.hunger}")
     def show(self):
         print(f"{self.name}, happiness:{self.happiness}, hunger:{self.hunger}")
+    def life(self):
+        self.alive = True
+        if self.alive != True:
+            print("Your pet is dead")
 
 Name = input("What would you like to name your pet?")
 Name = Pet(f"{Name}", 100, 50)
-Living = True
-
-if Name.hunger < 25:
-    print("Your pet is hungry feed him soon!")
-elif Name.hunger > 150:
-    print("Your pet is very full, stop feeding him!")
-elif Name.hunger > 200:
-    Living = False
-    print("Your pet died from overfeeding")
-elif Name.hunger == 0 or Name.hunger < 0:
-    Living = False
-    print("your pet died of hunger.")
 
 
 
-import random
-while Living == True:
-    randon_ = random.randint(1, 50)
-    if randon_ > 25:
-        Name.hunger -= 5
-    Options = input("What would you like to do? 1.Play  2.Feed")
-    if Options.lower() == "play":
-        Name.play(10)
-        Name.show()
-    elif Options.lower() == "feed":
-        Name.feed(10)
-        Name.show()
-                                              
+
+
+
+
+
