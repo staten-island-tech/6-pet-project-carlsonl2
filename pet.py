5c0bef339392ed7168fdbf7f19277522a1522343
carlsonl2 <carlsonl2@nycstudents.net>
2025-12-11 09:09:16 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -44,17 +44,18 @@ class Pet:
         self.happiness = happiness
         self.hunger = hunger
         self.alive = alive
-    def play(self):
-        self.happiness += 10
-        print(f"happiness has increased to {self.happiness}")
-    def feed(self):
-        self.hunger += 10
-        print(f"Your pets hunger increased to {self.hunger}")
+    def play(self, number):
+        self.happiness += number
+        print(f"happiness has changed to {self.happiness}")
+    def feed(self, number):
+        self.hunger += number
+        print(f"Your pets hunger changed to {self.hunger}")
+        if self.hunger == 0:
+            self.alive = False
         if self.hunger < 25:
             print("Your pet is hungry feed him soon!")
         elif self.hunger > 150:
              print("Your pet is very full, stop feeding him!")
-
     def show(self):
         print(f"{self.name}, happiness:{self.happiness}, hunger:{self.hunger}")
 
@@ -63,6 +64,14 @@ Name = input("What would you like to name your pet?")
 Name = Pet(f"{Name}", 100, 50, True)
 Name.show()
 while Name.alive == True:
+    import random
+    random.randint(1, 100)
+    if random.randint > 50:
+        Name.feed(-5)
+        Name.show()
+    elif random.randint < 50:
+        Name.happiness(-5)
+        Name.show()
     hungry = input("Here are the things you can do. \n"
                 "1: Play \n"
                 "2: Check Stats \n"
