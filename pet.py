a7838997195d97e2200460fded52ad47acee2ff0
carlsonl2 <carlsonl2@nycstudents.net>
2025-12-11 08:59:28 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,4 +1,4 @@
-class Pet:
+'''class Pet:
     def __init__(self, name, happiness, hunger, alive):
         self.name = name
         self.happiness = happiness
@@ -35,8 +35,46 @@ while Name.alive == True:
     if hungry.lower() == "feed":
         Name.feed(10)
     else:
-        print("Not an option")   
-                
+        print("Not an option") '''
+
+
+class Pet:
+    def __init__(self, name, happiness, hunger, alive):
+        self.name = name
+        self.happiness = happiness
+        self.hunger = hunger
+        self.alive = alive
+    def play(self):
+        self.happiness += 10
+        print(f"happiness has increased to {self.happiness}")
+    def feed(self):
+        self.hunger += 10
+        print(f"Your pets hunger increased to {self.hunger}")
+        if self.hunger < 25:
+            print("Your pet is hungry feed him soon!")
+        elif self.hunger > 150:
+             print("Your pet is very full, stop feeding him!")
+
+    def show(self):
+        print(f"{self.name}, happiness:{self.happiness}, hunger:{self.hunger}")
+
+
+Name = input("What would you like to name your pet?")
+Name = Pet(f"{Name}", 100, 50, True)
+Name.show()
+while Name.alive == True:
+    hungry = input("Here are the things you can do. \n"
+                "1: Play \n"
+                "2: Check Stats \n"
+                "3: Feed")
+    if hungry.lower() == "play":
+        Name.play()
+    elif hungry.lower() == "check stats":
+        Name.show()
+    elif hungry.lower() == "feed":
+        Name.feed()         
+    elif hungry.lower() != "play" or "check stats" or "feed":
+        print("Thats not an option")
 
 
 
